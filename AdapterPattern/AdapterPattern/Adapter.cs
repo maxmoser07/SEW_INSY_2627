7e53c702d5a01116cd01b4f0e3b9eca01f3bfa28
max <m.moser@htlkrems.at>
2026-09-14 12:54:16 +0200

    Adapter.cs added Methode ProcessPayment and added Adapter Constructor

--- AdapterPattern/AdapterPattern/Adapter.cs
+++ AdapterPattern/AdapterPattern/Adapter.cs
@@ -2,5 +2,17 @@ namespace AdapterPattern;
 
 public class Adapter : ITarget
 {
-    
+    private readonly LegacyPaymentGateway _legacyGateway;
+
+    public Adapter(LegacyPaymentGateway legacyGateway)
+    {
+        _legacyGateway = legacyGateway;
+    }
+
+    public void ProcessPayment(decimal amount)
+    {
+        // Translate: decimal euro -> string cents, and supply a currency code
+        string amountInCents = ((int)(amount * 100)).ToString();
+        _legacyGateway.MakeTransaction(amountInCents, "EUR");
+    }
 }
