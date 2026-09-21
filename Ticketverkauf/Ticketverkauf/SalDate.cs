302941885dc2c2f863e9039c0f73a083c5d243f1
max <m.moser@htlkrems.at>
2026-09-21 08:06:24 +0200

    added concurency check to SalDate.cs

--- Ticketverkauf/Ticketverkauf/SalDate.cs
+++ Ticketverkauf/Ticketverkauf/SalDate.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ticketverkauf.Api.Models;
 
+[Table("saldate")]
 public class SalDate
 {
     public int Id { get; set; }
     public string? Name { get; set; }
+
+    [ConcurrencyCheck]
     public float? Sal { get; set; }
 }
